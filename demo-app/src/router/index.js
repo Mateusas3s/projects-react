fce83b6812428f16777c129210c4e3c70e35aa5f
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2021-06-11 15:49:13 -0300

    Using Switch

--- demo-app/src/router/index.js
+++ demo-app/src/router/index.js
@@ -1,16 +1,22 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Switch, Route } from "react-router-dom"
 import Home from '../pages/Home'
 import About from "../pages/About"
 import Users from "../pages/Users"
 
 function AppRouter() {
     return(
-        <Router>
+        <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/users" component={Users}/>
-        </Router>
+        </Switch>
+
+        // <Router>
+        //     <Route exact path="/" component={Home} />
+        //     <Route exact path="/about" component={About} />
+        //     <Route exact path="/users" component={Users}/>
+        // </Router>
     )
 }
 export default AppRouter
