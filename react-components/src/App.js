d1464c4f5e7a55a8a120ccad8417b9b4228af97a
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2021-06-11 17:20:12 -0300

    Creating component button

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import Button from "./components/button/index"
+
 class App extends Component {
+
+  handleConfirm() {
+    alert("Confirmado!")
+  }
   render () {
     return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p> Hello World! </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button onClick={this.handleConfirm} link="https://medium.com/@henrique.weiand/react-propriedades-children-plano-de-aula-iv-488beb6ba94a">
+              <h4>Confirmar</h4>
+          </Button>
         </header>
       </div>
     )
